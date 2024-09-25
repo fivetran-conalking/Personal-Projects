debdc27062380e7e915ef3181dbe7741afe084e0
fivetran-jakedalton <87725120+fivetran-jakedalton@users.noreply.github.com>
2024-09-25 07:54:26 -0700

    Add files via upload

---
+++ Startup Script/startup_script.sh
@@ -0,0 +1,8 @@
+open https://fivetran.okta.com/app/zendesk/exk1g97g0gcnAtlBV1d8/sso/saml
+open https://fivetran.okta.com/app/fivetran_fivetranbackstage_1/exk1kl7z6f9VXXEPs1d8/sso/saml
+open https://fivetran.okta.com/home/bookmark/0oa1qxs7w8kPefGxC1d8/2557?fromPlugin=true
+gcloud auth application-default login --project metrics-and-alarms
+salame login
+brew update && brew upgrade
+cd ~/Fivetran/engineering && git pull
+cd ~
